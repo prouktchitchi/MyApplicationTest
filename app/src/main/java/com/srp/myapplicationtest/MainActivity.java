558928497437ac3a3f9a707fc81cea438bdaa893
prouktchitchi <36458676+prouktchitchi@users.noreply.github.com>
2018-02-14 12:35:18 +0100

    à_yiyuè

--- app/src/main/java/com/srp/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/srp/myapplicationtest/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    //newv uyftutftf iuyvuyf
+    //newv uyftutftf iuyvuyf utyt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
